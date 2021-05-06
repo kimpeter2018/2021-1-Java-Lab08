4cb8d111ca301e01d6589ef9917f51b9bf879715
Yertle <kimpeter2018@naver.com>
2021-05-06 16:41:57 +0900

    create IntegratedConverter.java + Util.java

---
+++ app/src/main/java/edu/handong/csee/java/hw3/Util.java
@@ -0,0 +1,12 @@
+package edu.handong.csee.java.hw3;
+
+/**
+ * this class contains repeatedly used method
+ */
+public class Util {
+
+    static String getUppercaseString(String stringValue) {
+        return stringValue.toUpperCase();
+    }
+    
+}
